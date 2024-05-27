1952dad22904c3dfef96208cf495b38ed1d3cef1
7vTr0x7 <7vTrox7@gmail.com>
2024-05-27 15:48:55 +0530

    PP2.4

--- PP2.4/script.js
+++ PP2.4/script.js
@@ -9,15 +9,23 @@ let count2Value = 0;
 let totalCountValue = 0;
 
 const initialValues = () => {
-    count1.
-}
+  count1.textContent = `Count 1: ${count1Value}`;
+  count2.textContent = `Count 2: ${count2Value}`;
+  totalCount.textContent = `Total Count: ${totalCountValue}`;
+};
+
+initialValues();
 
 count1Btn.addEventListener("click", () => {
   count1Value += 1;
-  count1.textContent = `${count1Value}`;
+  count1.textContent = `Count 1: ${count1Value}`;
+
+  totalCount.textContent = `Total Count: ${count1Value + count2Value}`;
 });
 
 count2Btn.addEventListener("click", () => {
   count2Value += 1;
   count2.textContent = `Count 2: ${count2Value}`;
+
+  totalCount.textContent = `Total Count: ${count1Value + count2Value}`;
 });
